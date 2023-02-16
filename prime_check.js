097d7d1dd327a1a2f0715e80a6b7551bc3ee5fd6
Shashikant kumar <loveuzindagi212@gmail.com.com>
2023-02-16 10:35:23 +0530

    added prime checker code

---
+++ prime_check.js
@@ -0,0 +1,14 @@
+let number=13;
+let count =0;
+
+for(let i=0; i<=number; i++){
+if(i%number==3){
+count++;
+}
+}
+if(count==2){
+console.log("It's prime");
+}
+else{
+console.log("It's not prime");
+}
