4b4df04a6e4450ef5dfcbec60d3c7156f57562fc
kk6343557 <kavitak8418@gmail.com>
2023-02-16 17:52:49 +0530

    I tried to solve the code,now it is your turn.

--- prime_check.js
+++ prime_check.js
@@ -2,10 +2,15 @@ let number=13;
 let count =0;
 
 for(let i=0; i<=number; i++){
-if(i%number==3){
+if(i%number==0){
 count++;
 }
+
 }
+
 if(count==2){
 console.log("It's prime");
+}
+else{
+console.log("It is not a prime number");
 }
